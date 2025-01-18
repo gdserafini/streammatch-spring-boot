6faddc7a9062f26e0366541f070360765bded8b4
gdserafini <gdserafini@outlook.com>
2025-01-18 13:19:02 -0300

    update -> run command line - process series

--- src/main/java/br/alura/streammatch/StreammatchApplication.java
+++ src/main/java/br/alura/streammatch/StreammatchApplication.java
@@ -1,13 +1,34 @@
 package br.alura.streammatch;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.alura.streammatch.model.SerieData;
+import br.alura.streammatch.service.APIConsumer;
+import br.alura.streammatch.service.DataConverter;
+
 @SpringBootApplication
-public class StreammatchApplication {
+public class StreammatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StreammatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		var consumer = new APIConsumer();
+		var converter = new DataConverter();
+		try{
+			var json = consumer.getData(
+				"https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c"
+			);
+			var data = converter.mapData(json, SerieData.class);
+			System.out.println(data);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 
 }
