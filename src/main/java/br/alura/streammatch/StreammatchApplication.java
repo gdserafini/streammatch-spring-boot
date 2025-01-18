91c0b923c64ee4822121ab2cf1852caac233f185
gdserafini <gdserafini@outlook.com>
2025-01-17 21:18:13 -0300

    first commit

---
+++ src/main/java/br/alura/streammatch/StreammatchApplication.java
@@ -0,0 +1,13 @@
+package br.alura.streammatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreammatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreammatchApplication.class, args);
+	}
+
+}
